edab1681dd36cd24359409748b4d64e282d1986b
canoklayy <can@vivoo.me>
2023-08-22 22:31:35 +0300

    first commit

---
+++ src/main/java/com/hepsiburada/App.java
@@ -0,0 +1,13 @@
+package com.hepsiburada;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/hepsiburada/AppTest.java
@@ -0,0 +1,21 @@
+package com.hepsiburada;
+
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
